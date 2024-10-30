58a13bd028659855b59497489c3df1f016ea3949
izaiasramos <izaiasramos123456789@gmail.com>
2024-10-30 20:56:18 -0300

    iniciando feature para listar produtos salvos

--- marca-listar.php
+++ marca-listar.php
@@ -1,1 +1,12 @@
 <h1>Listar Marca</h1>
+<?php
+
+$sql = "SELECT * FROM marca";
+$resultado = $conn->query($sql);
+$quantidade = $resultado->num_rows;
+
+if($quantidade > 0){
+    print "<p>Encontrou <b>$quantidade</b> resultado(s)</p>";
+}else{
+    print "Não foi encontrado resultados.";
+}
--- marca-salvar.php
+++ marca-salvar.php
@@ -1,1 +1,31 @@
-<h1>Salvar Marca</h1>
+// Inclua o arquivo de conexão
+<?php
+//include 'config.php'; // Altere para o caminho correto do seu arquivo de conexão
+
+switch ($_REQUEST['acao']) { // O switch recebe uma requisição que vem do input oculto com o name="acao"
+    case 'cadastrar':
+        $sql = "INSERT INTO marca
+                    (nome_marca)
+                VALUES
+                    ('".$_POST['nome_marca']."')";
+
+        $resultado = $conn->query($sql); // Execute a query   
+
+        if ($resultado==true) { // Se a query acima der certo
+            print "<script>alert('Cadastrado com sucesso!');</script>"; // Alerta ao usuário
+            print "<script>location.href='?page=marca-listar';</script>"; // Redireciona o usuário
+        } else {
+                print "<script>alert('Não foi possível cadastrar a marca!');</script>"; // Alerta de erro
+                print "<script>location.href='?page=marca-listar';</script>"; // Redireciona o usuário
+            }
+        break;
+   
+    case 'editar':
+        // Lógica para editar
+        break;
+
+    case 'excluir':
+        // Lógica para excluir
+        break;    
+}
+?>
