641de6fc067d535dab404164c279253cec8f3478
izaiasramos <izaiasramos123456789@gmail.com>
2024-10-31 07:26:16 -0300

    alterações no arquivo marca-listar

--- marca-listar.php
+++ marca-listar.php
@@ -7,6 +7,14 @@ $quantidade = $resultado->num_rows;
 
 if($quantidade > 0){
     print "<p>Encontrou <b>$quantidade</b> resultado(s)</p>";
+    print "<table>";
+        while($row = $resultado->fetch_object()){//fetch_object - busca/puxa os objetos/resultados no banco
+            print "<tr>";
+                    print "<td>".$row->id_marca."</td>";
+                    print "<td>".$row->nome_marca."</td>";
+            print "</tr>";
+        }
+    print "</table>";
 }else{
     print "Não foi encontrado resultados.";
 }
