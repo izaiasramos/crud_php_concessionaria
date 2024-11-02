5c6bac3a2654c50cc4ae1fc9dd6cf97ea5301f26
izaiasramos <izaiasramos123456789@gmail.com>
2024-11-02 18:21:04 -0300

    implementação de tabela em bootstrap para exibir dinamicamente produtos salvos no DB

--- marca-listar.php
+++ marca-listar.php
@@ -7,8 +7,12 @@ $quantidade = $resultado->num_rows;
 
 if($quantidade > 0){
     print "<p>Encontrou <b>$quantidade</b> resultado(s)</p>";
-    print "<table>";
-        while($row = $resultado->fetch_object()){//fetch_object - busca/puxa os objetos/resultados no banco
+    print "<table class='table table-bordered table-striped table-hover'>";
+            print "<tr>";
+                    print "<th>".'ID Marca'."</th>";
+                    print "<th>".'Nome Marca'."</th>";
+            print "</tr>"; 
+    while($row = $resultado->fetch_object()){//fetch_object - busca/puxa os objetos/resultados no banco
             print "<tr>";
                     print "<td>".$row->id_marca."</td>";
                     print "<td>".$row->nome_marca."</td>";
