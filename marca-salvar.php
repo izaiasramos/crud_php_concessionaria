4b7f1c409e2c41dbfb65fee482562a22dde18b85
izaiasramos <izaiasramos123456789@gmail.com>
2024-11-03 14:35:57 -0300

    feito lógica no arquivo marca salvar para receber edição feita pelo usuário e fazer um update

--- marca-salvar.php
+++ marca-salvar.php
@@ -22,7 +22,24 @@ switch ($_REQUEST['acao']) { // O switch recebe uma requisição que vem do inpu
    
     case 'editar':
         // Lógica para editar
-        break;
+        $sql = "UPDATE marca SET 
+                    nome_marca='".$_POST['nome_marca']."' 
+                WHERE 
+                    id_marca=".$_POST["id_marca"];//aqui já mando o parametro que vamos editar
+                //nome_marca do DB vai receber '".$_POST['nome_marca']."' o nome que vem do formulario            
+                //WHERE onde o id_marca id_marca=". tem que ser igual ao .$_POST["id_marca"]; id da marca presente na requisição/o id em questão/atual
+        
+        $resultado = $conn->query($sql); // Execute a query   
+
+        if ($resultado==true) { // Se a query acima der certo
+            print "<script>alert('Editado com sucesso!');</script>"; // Alerta ao usuário
+            print "<script>location.href='?page=marca-listar';</script>"; // Redireciona o usuário
+        } else {
+                print "<script>alert('Não foi possível editar a marca!');</script>"; // Alerta de erro
+                print "<script>location.href='?page=marca-listar';</script>"; // Redireciona o usuário
+            }       
+
+                break;
 
     case 'excluir':
         // Lógica para excluir
